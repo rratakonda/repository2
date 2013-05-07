eecb1829094a0d506c41868d1606b240bf206408
Radhika Ratakonda <rratakonda@liquidhub.com>
2013-05-07 19:03:42 +0530

    commit

--- index.php
+++ index.php
@@ -5,16 +5,14 @@ echo "testing gitextensions local commit"
 echo "Local commit";
 echo "Local commit";
 echo "testing gitextensions local commit"
-echo "Local commit";
-echo "Local commit";
-echo "testing gitextensions local commit"
-echo "Testing From another user ";
-echo "testing gitextensions local commit"
-echo "Local commit";
-echo "Local commit";
+
 echo "testing gitextensions local commit"
 #Resolve Conflicts
 
 # For Stash
+Testing stash
+testing123343
+Testing stash
+testing123343
  ?>
 
