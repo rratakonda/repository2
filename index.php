b1174c1a8c8cc85eeb59d08c52ea0822522a854e
Radhika Ratakonda <rratakonda@liquidhub.com>
2013-05-07 14:26:51 +0530

    commit from extensions

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 
 <?php echo "Hello world, Hello Git"
 echo "Testing From another user ";
+echo "testing gitextensions local commit"
 echo "Local commit";
 echo "Local commit";
+echo "testing gitextensions local commit"
  ?>
 
