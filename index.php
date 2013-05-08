db38a6ea70449afc490c04d5ccd36f34b9547d89
Radhika Ratakonda <rratakonda@liquidhub.com>
2013-05-08 15:15:59 +0530

    Commit from netbeans

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
 
-<?php echo "Hello world, Hello Git"
+<?php echo "Hello world, Hello Git";
 echo "Testing From another user ";
-echo "testing gitextensions local commit"
+echo "testing gitextensions local commit";
 echo "Local commit";
 echo "Local commit";
-echo "testing gitextensions local commit"
+echo "testing gitextensions local commit";
 
-echo "testing gitextensions local commit"
-#Resolve Conflicts
+echo "testing gitextensions local commit";
 
-# For Stash
-Testing stash
-testing123343
-Testing stash
-testing123343
  ?>
 
