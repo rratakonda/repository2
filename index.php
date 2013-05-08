aaba6d8c255bd66bd2075666cd5dd9af5eff84e0
Anil Kumar Bojja <akbojja5@gmail.com>
2013-05-08 12:46:01 +0530

    Resolved Conflicts

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
+
 <?php echo "Hello world, Hello Git"
 echo "Testing From another user ";
+echo "testing gitextensions local commit"
+echo "Local commit";
+echo "Local commit";
+echo "testing gitextensions local commit"
+echo "Local commit";
+echo "Local commit";
+echo "testing gitextensions local commit"
 echo "Testing From another user ";
 # "Testing From another user ";
 echo "testing gitextensions local commit"
@@ -8,4 +16,8 @@ echo "Local commit";
 echo "testing gitextensions local commit"
 echo 'Commiting from Git Extensions';
 # Resolve Conflicts
+#Resolve Conflicts
+
+#Stash1
  ?>
+
